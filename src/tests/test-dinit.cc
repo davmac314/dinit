cbd73e0e20931d2e4d614c336efacf69ff7129f3
Davin McCall <davmac@davmac.org>
2018-01-11 22:17:54 +0000

    Tests: use stubs for various functions.

---
+++ src/tests/test-dinit.cc
@@ -0,0 +1,15 @@
+#include "dasynq.h"
+
+using eventloop_t = dasynq::event_loop<dasynq::null_mutex>;
+
+eventloop_t event_loop;
+
+int active_control_conns = 0;
+
+void open_control_socket(bool report_ro_failure) noexcept
+{
+}
+
+void setup_external_log() noexcept
+{
+}
