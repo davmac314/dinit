b41d4ac7932576bab95637a4948d795234f7d8b8
Davin McCall <davmac@davmac.org>
2018-01-15 21:18:57 +0000

    baseproc-sys.h: Add explanatory comment.

--- src/includes/baseproc-sys.h
+++ src/includes/baseproc-sys.h
@@ -1,3 +1,10 @@
+/*
+ * This header implements a namespace, bp_sys, which wraps various system calls used by baseproc-service.cc.
+ *
+ * When running tests, another header is substituted in place of this one. The substitute provides mocks/stubs
+ * for the functions, to avoid calling the real functions and thus allow for unit-level testing.
+ */
+
 namespace bp_sys {
 
 using dasynq::pipe2;
