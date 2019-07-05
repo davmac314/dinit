7ccaeaaad3e5ca5f2b5a0a76fb103c80240f710d
Davin McCall <davmac@davmac.org>
2019-07-05 21:32:54 +1000

    Fix test stub for run_child_proc.

--- src/tests/test-run-child-proc.cc
+++ src/tests/test-run-child-proc.cc
@@ -4,10 +4,7 @@
 
 // Stub out run_child_proc function, for testing purposes.
 
-void base_process_service::run_child_proc(const char * const *args, const char *working_dir,
-        const char *logfile, bool on_console, int wpipefd, int csfd, int socket_fd,
-        int notify_fd, int forced_notify_fd, const char * notify_var, uid_t uid, gid_t gid,
-        const std::vector<service_rlimits> &rlimits) noexcept
+void base_process_service::run_child_proc(run_proc_params params) noexcept
 {
 
 }
