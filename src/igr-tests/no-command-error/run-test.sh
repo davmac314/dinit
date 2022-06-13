987999970a929addd3ba4a748df0ef4cb297006e
Davin McCall <davmac@davmac.org>
2022-06-13 21:33:32 +1000

    Fix no-command-error igr test if cgroup cannot be determined

--- src/igr-tests/no-command-error/run-test.sh
+++ src/igr-tests/no-command-error/run-test.sh
@@ -5,7 +5,14 @@
 
 rm -f dinit-run.log
 
-../../dinit -d sd -u -p socket -q \
+# If cgroups support, supply cgroup base path to avoid potential "unable to determine
+# cgroup" message
+CGROUPS_BASE=""
+if ../../dinit --version | grep -q " cgroups"; then
+    CGROUPS_BASE="-b \"\""
+fi
+
+../../dinit -d sd -u -p socket -q $CGROUPS_BASE \
 	no-command -l dinit-run.log
 
 STATUS=FAIL
