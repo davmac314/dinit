718c62ef9dab95b6465362584f39bb523b47765b
Mobin <mobin@mobintestserver.ir>
2022-09-26 23:08:11 +0330

    meson: add `ps-environ` integration test
    also fix: integration tests not runned on freebsd ci.
    
    Signed-off-by: Mobin <mobin@mobintestserver.ir>

--- src/igr-tests/ps-environ/run-test.sh
+++ src/igr-tests/ps-environ/run-test.sh
@@ -1,22 +1,29 @@
 #!/bin/sh
 
+if [ $IS_MESON ]; then
+   cd $(dirname $0)
+   DINIT_EXEC=$APPS_PATH/dinit
+else
+   DINIT_EXEC=../../dinit
+fi
+
 rm -f ./env-record
 
 export TEST_VAR_TWO=set-via-script
 
-../../dinit -d sd -u -p socket -q \
+$DINIT_EXEC -d sd -u -p socket -q \
 	checkenv1
 
-../../dinit -d sd -u -p socket -q \
+$DINIT_EXEC -d sd -u -p socket -q \
 	checkenv2
 
-../../dinit -d sd -u -p socket -q \
+$DINIT_EXEC -d sd -u -p socket -q \
 	checkenv3
 
-../../dinit -d sd -e dinit-environment -u -p socket -q \
+$DINIT_EXEC -d sd -e dinit-environment -u -p socket -q \
 	checkenv4
 
-../../dinit -d sd -u -p socket -q \
+$DINIT_EXEC -d sd -u -p socket -q \
 	checkenv4
 
 STATUS=FAIL
