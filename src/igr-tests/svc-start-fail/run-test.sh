d6e99ce3110fbb52b5f959bc86348e961062d947
Hojjat <mobin@mobintestserver.ir>
2022-11-20 21:57:47 +0330

    It's must be final!

--- src/igr-tests/svc-start-fail/run-test.sh
+++ src/igr-tests/svc-start-fail/run-test.sh
@@ -21,7 +21,7 @@ while [ ! -e socket ]; do
 done
 
 # try to start "bad-command" which will fail
-DINITCTLOUT="$($DINITCTL_EXEC -p socket start bad-command)"
+DINITCTLOUT="$($DINITCTL_EXEC -p socket start bad-command 2>&1)"
 if [ "$DINITCTLOUT" != "$(cat expected-1)" ]; then
     echo "$DINITCTLOUT" > actual-1
     kill $DINITPID; wait $DINITPID
@@ -29,14 +29,14 @@ if [ "$DINITCTLOUT" != "$(cat expected-1)" ]; then
 fi
 
 # try to start command which will timeout
-DINITCTLOUT="$($DINITCTL_EXEC -p socket start timeout-command)"
+DINITCTLOUT="$($DINITCTL_EXEC -p socket start timeout-command 2>&1)"
 if [ "$DINITCTLOUT" != "$(cat expected-2)" ]; then
     echo "$DINITCTLOUT" > actual-2
     kill $DINITPID; wait $DINITPID
     exit 1
 fi
 
-$DINITCTL_EXEC -p socket shutdown
+$DINITCTL_EXEC --quiet -p socket shutdown
 wait $DINITPID
 
 exit 0
