2f4198e0544d9de3a0b30a8345d27a5c7a55e7f1
Davin McCall <davmac@davmac.org>
2023-06-11 14:55:55 +1000

    igr test: further additions to before-after test

--- src/igr-tests/before-after/run-test.sh
+++ src/igr-tests/before-after/run-test.sh
@@ -20,16 +20,16 @@ sleep 0.2
 # start parent; should start service2 and then service1 (due to before= in service2).
 "$DINITCTL_EXEC" --quiet -p socket start parent
 
-
 # Note service1 takes longer to start, but has a "before" service2 so should still start first.
-if [ "$(cat output/script-output)" != "$(printf "one\ntwo\n")" ]; then
+# service3 is similarly "after" service2.
+if [ "$(cat output/script-output)" != "$(printf "one\ntwo\nthree\n")" ]; then
     "$DINITCTL_EXEC" --quiet -p socket shutdown
     return 1
 fi
 
 rm output/script-output
 
-# unloading and reloading service2 should not lose the before= relationship
+# unloading and reloading service2 should not lose the before= or after= relationship
 "$DINITCTL_EXEC" --quiet -p socket stop parent
 "$DINITCTL_EXEC" --quiet -p socket unload parent
 "$DINITCTL_EXEC" --quiet -p socket unload service2
@@ -37,7 +37,7 @@ rm output/script-output
 "$DINITCTL_EXEC" --quiet -p socket reload service2
 "$DINITCTL_EXEC" --quiet -p socket start parent
 
-if [ "$(cat output/script-output)" != "$(printf "one\ntwo\n")" ]; then
+if [ "$(cat output/script-output)" != "$(printf "one\ntwo\nthree\n")" ]; then
     "$DINITCTL_EXEC" --quiet -p socket shutdown
     return 1
 fi
