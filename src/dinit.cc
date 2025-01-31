6be8175ef4c74e0753d89f2bd6eca1341ef0e27e
Rahul Sandhu <rahul@sandhuservices.dev>
2025-01-31 17:53:05 +0000

    selinux_transition: update comment wording to note relevant manpage
    Signed-off-by: Rahul Sandhu <rahul@sandhuservices.dev>

--- src/dinit.cc
+++ src/dinit.cc
@@ -545,10 +545,9 @@ static bool selinux_transition(const char *exe)
     // the xattrs for `exe`. This is a policy choice, and not a dinit runtime issue. Let's continue
     // the boot process regardless, but still log a warning where applicable.
 
-    // The following comment is loosely based on the getcon(3) manpage from the SELinux Project.
-    // getcon_raw(3) may return 0 indicating success and set current_context to NULL if SELinux is
-    // disabled, or other LSMs are at play. It's best to check the pointer we get back in addition
-    // to the return value.
+    // As indicated by the getcon(3) manpage, getcon_raw(3) may return 0 indicating success and set
+    // current_context to NULL if SELinux is disabled, or other LSMs are at play. It's best to
+    // check the pointer we get back in addition to the return value.
     if (getcon_raw(&current_context) < 0 || current_context == nullptr) {
         cerr << "Failed to get current context: " << strerror(errno) << endl;
         goto cleanup;
