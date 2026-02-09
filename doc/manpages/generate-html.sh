87731bd03be76df43b9c88746a07140d2238278f
Davin McCall <davmac@davmac.org>
2026-02-10 07:20:42 +1000

    man pages: improve generated CSS for HTML versions
    Fix some formatting issues by tweaking the CSS for the HTML man pages.

--- doc/manpages/generate-html.sh
+++ doc/manpages/generate-html.sh
@@ -7,10 +7,11 @@ body    { margin-left: auto; margin-right: auto; width: 60em; padding-left: 1em;
 body > h1:first-child { margin-top: 0; }
 html    { background: darkslategray }  a { color: inherit; text-decoration: none }
 section > p { margin-left: 3em; }
+section > ul { margin-left: 3em; }
 table.head, table.foot { width: 100%; }
 td.head-rtitle, td.foot-os { text-align: right; }
 td.head-vol { text-align: center; }
-dd { margin-left: 3em; }
+dd { margin-left: 3em; margin-bottom: 1em; }
 .Bd-indent { margin-left: 3em; }
 .Nd, .Bf, .Op { display: inline; }
 .Pa, .Ad { font-style: italic; }
